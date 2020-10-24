4b2575756becb227de8558a562819a2e0a945e8f
Amit Sakharkar <amit@smilebots.com>
2020-10-24 15:42:26 +0530

    few more changes applied how to play added.

--- src/components/chidiyaUdd.js
+++ src/components/chidiyaUdd.js
@@ -4,7 +4,7 @@ const styles = (theme) => ({
   animalImage: {
     width: 200,
     height: 200,
-    borderRadius: '50px',
+    borderRadius: '50%',
   },
   mainDiv: {
     display: 'flex',
@@ -15,12 +15,29 @@ const styles = (theme) => ({
     width: '100wh',
     height: '100vh',
   },
+  howToPlay: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '60%',
+  },
+  scoreDisplay: {
+    display: 'flex',
+    width: '60%',
+    justifyContent: 'space-between',
+    cursor: 'pointer',
+  },
+  textStyle: { color: 'white', fontSize: '2rem', fontWeight: 'bold' },
 });
 class ChidiyaUdd extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      score: 0,
+      counter: 0,
       isPlaying: false,
+      displayHowToPlay: false,
+      flyableAnimals: [],
+      nonFlyableAnimals: [],
     };
   }
 
@@ -32,51 +49,122 @@ class ChidiyaUdd extends Component {
     }
   }
 
-  randomImageTitle(min, max) {
+  randomImageTitle = (min, max) => {
     // will retrieve random image file name
-    return `${Math.floor(Math.random() * (max - min + 1)) + min}.jpg`;
-  }
-  generateRandomImage() {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+  generateRandomImage = () => {
     setInterval(() => {
-      document.getElementById('randomImage').src = `${this.randomImageTitle(
-        1,
-        155
-      )}`;
-    }, Math.floor(Math.random() * (2000 - 10 + 1000)) + 10);
-  }
+      if (this.state.isPlaying) {
+        document.getElementById('randomImage').src = `${this.randomImageTitle(
+          1,
+          158
+        )}.jpg`;
+        this.setState({ counter: this.state.counter + 1 });
+      } else {
+        return 1;
+      }
+    }, this.randomImageTitle(1500, 2000));
+  };
   setPlayingStatus = (status) => {
+    if (status === true) {
+      this.setState({ score: 0, couter: 0 }, () => this.generateRandomImage());
+    } else {
+      let score = this.state.counter * 50;
+      this.setState({ score });
+    }
     this.setState({ isPlaying: status });
+  };
+  handleImageClick = (evt) => {
+    let fileNameData = evt.target.src.split('/');
+    let fileName = fileNameData[3].split('.');
+    let animalImageFile = fileName[0];
+    if (
+      (animalImageFile > 1 && animalImageFile < 19) ||
+      (animalImageFile > 19 && animalImageFile < 81) ||
+      animalImageFile === 84 ||
+      animalImageFile === 88 ||
+      animalImageFile === 105
+    ) {
+      return 1;
+    } else {
+      this.setPlayingStatus(false);
+    }
+  };
+  setDisplayHowToPlay = (status) => {
+    this.setState({ displayHowToPlay: status });
   };
   render() {
     const { classes } = this.props;
     return (
       <div className={classes.mainDiv}>
-        {this.state.isPlaying ? (
+        {this.state.displayHowToPlay ? (
+          <div className={classes.howToPlay}>
+            <Typography className={classes.textStyle}>
+              Just click on the birds images, but if clicked on animal image the
+              game will end.
+            </Typography>
+            <Typography
+              className={classes.textStyle}
+              onClick={(evt) => {
+                evt.preventDefault();
+                this.setDisplayHowToPlay(false);
+              }}
+            >
+              Go Back
+            </Typography>
+          </div>
+        ) : this.state.isPlaying ? (
           <>
             <img
               id="randomImage"
               className={classes.animalImage}
-              src=""
+              src={`${this.randomImageTitle(1, 158)}.jpg`}
               alt="temp"
-            />
-            <Typography
               onClick={(evt) => {
                 evt.preventDefault();
-                this.setPlayingStatus(false);
+                this.handleImageClick(evt);
               }}
-            >
-              Quit
-            </Typography>
+            />
+            <div className={classes.scoreDisplay}>
+              <Typography className={classes.textStyle}>
+                Your score : {this.state.score}
+              </Typography>
+              <Typography
+                className={classes.textStyle}
+                onClick={(evt) => {
+                  evt.preventDefault();
+                  this.setPlayingStatus(false);
+                }}
+              >
+                Quit
+              </Typography>
+            </div>
           </>
         ) : (
-          <Typography
-            onClick={(evt) => {
-              evt.preventDefault();
-              this.setPlayingStatus(true);
-            }}
-          >
-            Start
-          </Typography>
+          <div className={classes.scoreDisplay}>
+            <Typography
+              className={classes.textStyle}
+              onClick={(evt) => {
+                evt.preventDefault();
+                this.setDisplayHowToPlay(true);
+              }}
+            >
+              How to play
+            </Typography>
+            <Typography className={classes.textStyle}>
+              Your score : {this.state.score}
+            </Typography>
+            <Typography
+              className={classes.textStyle}
+              onClick={(evt) => {
+                evt.preventDefault();
+                this.setPlayingStatus(true);
+              }}
+            >
+              Start
+            </Typography>
+          </div>
         )}
       </div>
     );
